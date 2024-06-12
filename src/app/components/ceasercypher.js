fcc30b02938d6f96809910d21d87f299db267cc9
Saihaj Mann <saihajmann07@gmail.com>
2024-06-11 20:52:21 -0600

    basic format

--- src/app/components/ceasercypher.js
+++ src/app/components/ceasercypher.js
@@ -1,8 +1,18 @@
+import { useState } from 'react';
+
 export default ceaserCypher = () => {
-    const Encypt = () => {
-        
+    const [Edisplay, EsetDisplay] = useState('');
+    const [Ddisplay, DsetDisplay] = useState('');
+    const Encypt = (value) => {
+        EsetDisplay(value)
+    }
+    const Decypt = (value) => {
+        DsetDisplay(value)
     }
     return(
-        <div>Ceaser Cypher</div>
+        <div>
+            <h2>Ceaser Cypher</h2>
+
+        </div>
     )
 }
