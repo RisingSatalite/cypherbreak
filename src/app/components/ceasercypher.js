0b4331da8940a1d64be681822e5c08a7cccc0e2d
Saihaj Mann <saihajmann07@gmail.com>
2024-06-15 21:17:32 -0600

    changes

--- src/app/components/ceasercypher.js
+++ src/app/components/ceasercypher.js
@@ -32,27 +32,36 @@ export default function ceaserCypher(){
 
     const Encypt = (value, shift) => {
         EsetDisplay(caesarShift(value,shift))
+        if(value === ""){
+            EsetDisplay("Enter a value for Encypt first")
+        }
     }
     const Decypt = (value, shift) => {
         DsetDisplay(caesarShift(value, -shift))
+        if(value === ""){
+            DsetDisplay("Enter a value for Decypt first")
+        }
     }
     return(
         <div>
-            <input 
-                type="number" 
-                min="1" 
-                max="25" 
-                step="1"
-                value={shift} 
-                onChange={(e) => setShift(e.target.value)}
-                placeholder="Select shift value"
-            />
             <h2>Ceaser Cypher</h2>
+            <div>
+                <input 
+                    type="number" 
+                    min="1" 
+                    max="25" 
+                    step="1"
+                    value={shift} 
+                    onChange={(e) => setShift(e.target.value)}
+                    placeholder="Select shift value"
+                />
+                <span>Cypher Shift</span>
+            </div>
             <input 
                 type="text" 
                 value={encyptInput} 
                 onChange={(e) => setEcyptInput(e.target.value)}
-                placeholder="Enter text"
+                placeholder="Enter text to encypt"
             />
             <button onClick={() => Encypt(encyptInput, shift)}>
                 <div>Encypt</div>
@@ -62,7 +71,7 @@ export default function ceaserCypher(){
                 type="text" 
                 value={decyptInput} 
                 onChange={(e) => setDecyptInput(e.target.value)}
-                placeholder="Enter converted" 
+                placeholder="Enter text to decrypt" 
             />
             <button onClick={() => Decypt(decyptInput, shift)}>
                 <div>Decypt</div>
