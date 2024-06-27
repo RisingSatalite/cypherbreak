00a5cc7bd7e5422ed3450c658ede6018ecce3061
Saihaj Mann <saihajmann07@gmail.com>
2024-06-27 13:36:44 -0600

    changes

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,15 @@
 import Image from "next/image";
 import Ceasercypher from "./components/ceasercypher";
 import AffineCypher from "./components/affinecypher";
+import CeaserCypherInfo from "./components/ceasercypherinfo";
+import Nav from "./components/nav";
 
 export default function Home() {
   return (
     <main className="flex flex-col items-center justify-between">
       <div class="p-10">Cypher Break</div>
       <Ceasercypher></Ceasercypher>
+      <CeaserCypherInfo></CeaserCypherInfo>
       <AffineCypher></AffineCypher>
     </main>
   );
