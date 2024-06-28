a73a65aff332395c3886fdb1bcaecd663b78645c
Saihaj Mann <saihajmann07@gmail.com>
2024-06-27 22:49:21 -0600

    Update ceasercypherinfo.js
    fix ' problem

--- src/app/components/ceasercypherinfo.js
+++ src/app/components/ceasercypherinfo.js
@@ -7,7 +7,7 @@ It is a type of substitution cipher in which each letter in the plaintext is rep
 For example, with a left shift of 3, D would be replaced by A, E would become B, and so on.
 The Caesar cipher is named after Julius Caesar, who used it in his private correspondence.
 Evidence exists that Julius Caesar also used more complicated systems.
-Caesar ciphers can be found today in children's toys such as secret decoder rings.
+Caesar ciphers can be found today in childrens toys such as secret decoder rings.
             </p>
         </div>
     )
