790070767f22340f7320c8005030c764865787ae
Saihaj Mann <saihajmann07@gmail.com>
2024-07-31 00:28:43 -0600

    Create page.js
    new pages

---
+++ src/app/ceaser/page.js
@@ -0,0 +1,15 @@
+'use client'
+
+import Image from "next/image";
+import Ceasercypher from "../components/ceasercypher";
+import CeaserCypherInfo from "../components/ceasercypherinfo";
+
+export default function Home() {
+  return (
+    <main className="flex flex-col items-center justify-between">
+      <div class="p-10">Cypher Break</div>
+      <Ceasercypher></Ceasercypher>
+      <CeaserCypherInfo></CeaserCypherInfo>
+    </main>
+  );
+}
